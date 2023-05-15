683334929dcdd4423b7f359a45a7ec0058d641b6
manzitresor <83731517+manzitresor@users.noreply.github.com>
2023-05-15 19:15:56 +0200

    Add mobile styles and script for open and close menu

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-function openMenu() {
-    document.getElementById("nav-list").style.display = "block";
-}
+const openBtn = document.getElementById('menu-btn');
+const menuContain = document.querySelector('.menu-container');
+const mobileMenu = document.querySelector('.mobile-menu');
+const closeBtn = document.querySelector('.close-btn');
+const listMenu = document.querySelector('.list-menu');
+openBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+  mobileMenu.style.display = 'block';
+  menuContain.style.display = 'none';
+});
+
+closeBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+  menuContain.style.display = 'flex';
+  mobileMenu.style.display = 'none';
+});
+
+listMenu.addEventListener('click', (event) => {
+  event.preventDefault();
+  menuContain.style.display = 'flex';
+  mobileMenu.style.display = 'none';
+});
